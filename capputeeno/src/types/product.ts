5888d8e31ff484f2f11812bf5016f7c1f6a39f16
rafalmeidas <rafalmeidas21@gmail.com>
2023-12-24 16:53:02 -0300

    feat: add types

---
+++ capputeeno/src/types/product.ts
@@ -0,0 +1,18 @@
+export interface Product {
+  id: string;
+  name: string;
+  price_in_cents: number;
+  image_url: string;
+  category: string;
+  description?: string;
+}
+
+export interface ProductInCart extends Product {
+  quantity: number;
+}
+
+export interface ProductFetchResponse {
+  data: {
+    Product: Product;
+  };
+}
