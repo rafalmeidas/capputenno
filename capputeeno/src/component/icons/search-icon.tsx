4ac245a84731764b21bc55fc0b1ae3018462b1fc
rafalmeidas <rafalmeidas21@gmail.com>
2023-12-24 16:48:44 -0300

    feat: add icons

---
+++ capputeeno/src/component/icons/search-icon.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Searchicon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="26"
+      height="26"
+      fill="none"
+      viewBox="0 0 26 26"
+    >
+      <circle
+        cx="12.059"
+        cy="12.059"
+        r="7.062"
+        stroke="#737380"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      />
+      <path
+        stroke="#737380"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="m21.003 21.003-3.951-3.951"
+      />
+    </svg>
+  );
+}
